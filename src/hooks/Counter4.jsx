d65b12e1375e63c2cf18ee1ad3c56c847a7ff1d7
Gbolahan2003 <matthewarowosegbe4@gmail.com>
2023-11-13 23:20:03 +0100

    zdgtyfgj

---
+++ src/hooks/Counter4.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Counter4() {
+    const [counter1, setCounter1] = useState()
+    const [counter2, setCounter2] = useState()
+  return (
+    <div>Counter4</div>
+  )
+}
+
+export default Counter4
